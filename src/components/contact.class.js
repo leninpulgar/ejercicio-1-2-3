d6646083f3567e76f52d183991751efe128db72c
Lenin Pulgar <lenindpulgar@gmail.com>
2022-08-31 17:06:35 -0400

    corrección

--- src/components/contact.class.js
+++ src/components/contact.class.js
@@ -1,17 +1,3 @@
-/* export class ContactClass {
-    name = '';
-    lastname = '';
-    email = '';
-    online = false;
-
-    constructor(name, lastname, email, online){
-        this.name = name;
-        this.lastname = lastname;
-        this.email = email;
-        this.online = online;
-    }
-} */
-
 export class ContactClass {
     name = '';
     lastname = '';
